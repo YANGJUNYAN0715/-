b62e4e1f641b44b4bfb9e0fa0bdc85ed2f7c5933
YANGJUNYAN0715 <1310204834@qq.com>
2022-08-04 23:15:09 +0800

    2022.8.4

--- express/路由模块/模块化路由.js
+++ express/路由模块/模块化路由.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const router = require("./router1");
-app.use(router);
+app.use("/api", router);
 
 app.listen(80, () => {
   console.log("http://127.0.0.1");
